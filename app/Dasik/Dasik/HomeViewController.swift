7d14d4a4952805e433a2c1acdaa669f755f142d4
희준이 <heejun@huijun-iui-MacBookPro.local>
2021-05-27 16:10:59 +0900

    seg

---
+++ app/Dasik/Dasik/HomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  HomeViewController.swift
+//  Dasik
+//
+//  Created by 희준이 on 2021/05/26.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet var RecomandButtons: [UIButton]!
+    @IBOutlet weak var dietProgress: UIProgressView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+    @IBAction func RecomandButtonTapped(_ sender: Any) {
+    }
+}
