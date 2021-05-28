b986aed8785145fc23866cca2f05254c5562aafc
한현진 <hjhan@hanhyeonjin-ui-MacBookPro.local>
2021-05-28 23:18:34 +0900

    menu_modify

--- app/Dasik/Dasik/HomeView/HomeViewController.swift
+++ app/Dasik/Dasik/HomeView/HomeViewController.swift
@@ -8,14 +8,8 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
-    @IBOutlet var RecomandButtons: [UIButton]!
-    @IBOutlet weak var dietProgress: UIProgressView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-    }
-    @IBAction func RecomandButtonTapped(_ sender: Any) {
+    // Do any additional setup after loading the view.
     }
 }
