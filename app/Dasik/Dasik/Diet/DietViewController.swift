87b8fe50e515d479e0af6cf85455874a973a33b3
희준이 <heejun@huijun-iui-MacBookPro.local>
2021-06-03 18:29:55 +0900

    calendar

--- app/Dasik/Dasik/Diet/DietViewController.swift
+++ app/Dasik/Dasik/Diet/DietViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class DietiewController: UIViewController {
+class DietViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     // Do any additional setup after loading the view.
