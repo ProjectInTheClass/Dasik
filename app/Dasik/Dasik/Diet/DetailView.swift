353275f8faff0905475c5f30a5ab6e0216aa25f4
희준이 <heejun@huijun-iui-MacBookPro.local>
2021-06-05 18:08:06 +0900

    message

--- app/Dasik/Dasik/Diet/DetailView.swift
+++ app/Dasik/Dasik/Diet/DetailView.swift
@@ -9,7 +9,22 @@ import SwiftUI
 
 struct DetailView: View {
     var body: some View {
-        Text(dateSelected)
+        //Text(dateSelected)
+        VStack {
+            Text("식단표")
+                .font(.title)
+                .position(x: 150 ,y: 20)
+            Text("아침")
+                .font(.title)
+                .position(x: 30, y: -70)
+            Text("점심")
+                .font(.title)
+                .position(x: 30, y: -90)
+            Text("저녁")
+                .font(.title)
+                .position(x: 30, y: -90)
+        }
+            
     }
 }
 
--- app/Dasik/Dasik/Setting/SettingViewController.swift
+++ app/Dasik/Dasik/Setting/SettingViewController.swift
@@ -8,7 +8,13 @@
 import UIKit
 
 class SettingViewController: UIViewController {
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var userHeightLabel: UILabel!
+    @IBOutlet weak var userWeightLabel: UILabel!
     override func viewDidLoad() {
+        userNameLabel.text = TmpUser.name
+        userWeightLabel.text = String(TmpUser.weight)
+        userHeightLabel.text = String(TmpUser.height)
         super.viewDidLoad()
     // Do any additional setup after loading the view.
     }
