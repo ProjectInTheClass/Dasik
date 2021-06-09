b87ea36e2b6546f773d1c48d780f963d7226bee1
hjhan1998 <61860908+hjhan1998@users.noreply.github.com>
2021-06-09 22:47:53 +0900

    Delete app directory

--- app/Dasik/Dasik/Rank/DetailsRankViewController.swift
+++
@@ -1,8 +0,0 @@
-//
-//  DetailsRankViewController.swift
-//  Dasik
-//
-//  Created by 이현민 on 2021/06/03.
-//
-
-import Foundation
--- app/Dasik/Dasik/Rank/File.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  Dasik
-//
-//  Created by 이현민 on 2021/06/03.
-//
-
-import Foundation
